c627a0481e7535144b86be85c6745169b4411d46
Fatima Waheed <167896135+Fatima-Waheed0@users.noreply.github.com>
2025-04-10 16:07:11 +0500

    Update main.py

---
+++ scripts/main.py
@@ -0,0 +1,12 @@
+from scripts.train_model import train_model
+from scripts.evaluate_model import evaluate_model
+
+def main():
+    # Train the model
+    train_model()
+
+    # Evaluate the model
+    evaluate_model()
+
+if __name__ == "__main__":
+    main()
